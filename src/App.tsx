2c65c4dca2d15445ce5a7d7dfd6ceec53159676b
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-02-17 07:53:40 -0500

    portfolio grid design

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="header">Header</header>
+      <main className="main_content">Content</main>
+      <aside className="sidebar">Sidebar</aside>
+      <footer className="footer">Footer</footer>
+    </>
+  );
+}
+
+export default App;
