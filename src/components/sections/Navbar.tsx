de06314781bc3058a8fa902509893132c0f7e841
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-02-29 07:56:07 -0500

    add  transition to navbar section

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -3,12 +3,10 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <section className={styles.navbar}>
-      <ol>
         <li>About</li>
         <li>Technologies</li>
         <li>Project</li>
         <li>Contact</li>
-      </ol>
     </section>
   );
 };
