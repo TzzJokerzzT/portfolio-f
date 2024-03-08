04a120aaa8a9c3fbcc4a4ac7565cd102cc79d101
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-03-08 07:23:11 -0500

    add flex on techs comp

--- src/components/sections/Techs.tsx
+++ src/components/sections/Techs.tsx
@@ -7,26 +7,32 @@ const Techs = () => {
       </div>
       <section>
         <h2>Languages:</h2>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Javascript</li>
+        <div>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+        </div>
       </section>
       <section>
         <h2>Frameworks:</h2>
-        <li>Astro</li>
-        <li>React</li>
-        <li>Next.js</li>
-        <li>TailwindCSS</li>
+        <div>
+          <li>Astro</li>
+          <li>React</li>
+          <li>Next.js</li>
+          <li>TailwindCSS</li>
+        </div>
       </section>
       <section>
         <h2>Dev Tools:</h2>
-        <li>Git</li>
-        <li>Bootsrap</li>
-        <li>Next UI</li>
-        <li>Netlify</li>
-        <li>Typescript</li>
-        <li>Mamba UI</li>
-        <li>Vite</li>
+        <div>
+          <li>Git</li>
+          <li>Bootsrap</li>
+          <li>Next UI</li>
+          <li>Netlify</li>
+          <li>Typescript</li>
+          <li>Mamba UI</li>
+          <li>Vite</li>
+        </div>
       </section>
     </section>
   );
