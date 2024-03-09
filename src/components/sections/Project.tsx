fc4a2bdffc9dca941bf8fd33082627c740aed835
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-03-09 07:41:25 -0500

    adding projec position styles

--- src/components/sections/Project.tsx
+++ src/components/sections/Project.tsx
@@ -1,12 +1,30 @@
+import style from "./Project.module.css";
+
 const Project = () => {
   return (
-    <section>
-      <h1>My personal projects</h1>
-      <li>Rick & Morty App</li>
-      <li>Weather App</li>
-      <li>Bankist</li>
-      <li>Guess the Number</li>
-      <li>Chuck Norris Phrases</li>
+    <section className={style.project}>
+      <div>
+        <h1>My personal projects</h1>
+      </div>
+      <div className={style.card}>
+        <img
+          src="/img/Fiora_Portrait.jpg"
+          alt="Project Image"
+          className={style.card_img}
+        />
+        <div className={style.card_content}>
+          <h2 className={style.card_title}>Card Title</h2>
+          <p className={style.card_text}>Content</p>
+          <a href="#" className={style.card_link}>
+            Enlace
+          </a>
+        </div>
+        {/* <li>Rick & Morty App</li>
+        <li>Weather App</li>
+        <li>Bankist</li>
+        <li>Guess the Number</li>
+        <li>Chuck Norris Phrases</li> */}
+      </div>
     </section>
   );
 };
